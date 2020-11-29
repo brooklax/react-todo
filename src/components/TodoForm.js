ebe95eb213f1c9292adfd3d45e4b8217ae28894d
Gideon <godz@Gideons-MacBook-Pro.local>
2020-11-29 16:18:16 +0200

    add Todo components

---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => inputRef.current.focus())
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input,
+        });
+        setInput('');
+    }
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="todo-form">
+            {props.edit ? (
+                    <>
+                    <input type="text" placeholder="Update a todo" value={input} name="text" className="todo-input edit" onChange={handleChange} ref={inputRef} />
+                    <button className="todo-button">Update</button>
+                    </>
+                ) : (
+                    <>
+                    <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange} ref={inputRef} />
+                    <button className="todo-button">Add Todo</button>
+                    </>
+                )
+            }
+        </form>
+    )
+}
+
+export default TodoForm
